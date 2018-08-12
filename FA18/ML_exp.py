112e4bc56ca277fbddef46e4b10ce58b5d625c8f
Siyang Jing <siyangj@live.unc.edu>
2018-08-12 19:20:15 -0400

    ...

--- FA18/ML_exp.py
+++ FA18/ML_exp.py
@@ -18,7 +18,9 @@ if __name__=="__main__":
     data_arr = pickle.load(f)
     f.close()
     f = open(args.pickle_dir,'wb+') 
+    print(data_arr)
     np.append(data_arr,[[args.RSEED,args.sigmaobs,mae,acc]],axis=0)
+    print(data_arr)
     pickle.dump(data_arr,f)
     f.close()
     
